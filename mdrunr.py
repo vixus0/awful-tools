e2ebbc13a7ba5ea577f5d10b6c4ed21c6dc81448
Pascal Bugnion <pob24@cam.ac.uk>
2013-02-02 17:10:22 +0000

    Added ability to run non-mpi jobs.
    Specifying the "mpi_mode" flag in the "Mdjob" instantiation allows
    running a command that does not start with "mpiexec".
    
    Jobs are assumed to be "mpi" jobs by default, for
    backwards-compatibility reasons.

--- mdrunr.py
+++ mdrunr.py
@@ -25,31 +25,40 @@ flog=open("/tmp/mdrunr.log", "w")
 class Mdjob :
   """Defines a job to run."""
 
-  def __init__(self, name, args):
+  def __init__(self, name, args, mpi_mode=True):
     """Create a job.
 
     Arguments are name and an argument list (use shlex.split!).
     """
     self.name = name
     self.args = args
+    self.mpi_mode = mpi_mode
+
+  def run(self,np_per):
+    if self.mpi_mode: 
+      md_job = ["mpiexec", "-n", np_per] + self.args
+    else: 
+      md_job = self.args
+    sp.call(md_job, stdout=fnull, stderr=flog)
 
 class runner (thr.Thread):
-  def __init__(self, queue, np_per):
+  def __init__(self, queue, np_per,mpi_mode=True):
     thr.Thread.__init__(self)
     self.my_queue = queue
     self.np_per = np_per
+    self.mpi_mode = mpi_mode
 
   def run(self):
     while True:
       try:
         my_job = self.my_queue.get(True,2)
-      except:
+      except qu.Empty:
         return
       else:
         if my_job:
-          md_job = ["mpiexec", "-n", `self.np_per`] + my_job.args
           print "Starting job: ", my_job.name
-          sp.call(md_job, stdout=fnull, stderr=flog)
+          my_job.run(self.np_per)
+          print "Done: ", my_job.name
           time.sleep(.5)
           self.my_queue.task_done()
 
@@ -57,12 +66,18 @@ class runner (thr.Thread):
 class Mdrunr :
   """Sets up and runs a queue."""
 
-  def __init__(self, md_args_list, np_per):
+  def __init__(self, md_args_list, np_per, np_tot=None):
     """Initialise the queue.
 
-    Arguments are a list of Mdjobs and the number of processors per job.
+    :Parameters:
+        - `md_args_list` : MdJob instances describing the jobs.
+        - `np_per` : number of processes to use per job.
+        - `np_tot` : total number of processors to use.
     """
-    self.np_tot   = mp.cpu_count()
+    if np_tot is None:
+        self.np_tot   = mp.cpu_count()
+    else:
+        self.np_tot = np_tot
 
     if np_per > self.np_tot:
       print "You can't specify more than %d processors per job!"%(self.np_tot)
@@ -76,19 +91,19 @@ class Mdrunr :
   def start(self):
     """Start submitting jobs from the queue."""
     try:
-      for i in range(self.nthr):
-        t = runner(self.queue, self.np_per)
-        t.daemon = True
-        t.start()
 
       for x in self.jobs:
         print "Added job: ", x.name
         self.queue.put(x)
       
-      print "Running..",
-      while self.queue.empty() == False:
-        time.sleep(1)
+      for i in range(self.nthr):
+        t = runner(self.queue, self.np_per)
+        t.daemon = True
+        t.start()
+
+      self.queue.join()
       print "done!"
     except (KeyboardInterrupt, SystemExit):
       print "Force quit!"
+
 
